6eef7dfeb0ece591cd55ded9c164390690170a4d
AteqEjaz <ateq.odesk@gmail.com>
2017-11-22 03:37:27 +0500

    Buying and selling multiple articles has been completed.

--- Training/chainlist/truffle.js
+++ Training/chainlist/truffle.js
@@ -3,7 +3,12 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: '*' // Match any network id
+    },
+    rinkbey: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4 // rinkbey network id
     }
   }
 };
